9c949601e227a7d01802929fbdfcd68dec649692
Hoang Vu <nguyenhoangvu1803@gmail.com>
2016-03-11 22:20:17 +0700

    up

--- wp-content/themes/fajar/inc/header/header.php
+++ wp-content/themes/fajar/inc/header/header.php
@@ -28,10 +28,10 @@
 			<div id="yeah-header-logo" class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img alt="" src="<?php echo esc_url(zo_page_header_logo()); ?>"></a>
 			</div>
-			<div id="yeah-header-banners" class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
+			<div id="yeah-header-banners" class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
 				<?php if(is_active_sidebar('header-baners')){ dynamic_sidebar('header-baners'); } ?>
 			</div>
-			<div id="yeah-header-cart" class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
+			<div id="yeah-header-cart" class="col-xs-12 col-sm-1 col-md-1 col-lg-1">
 				<?php if(is_active_sidebar('sidebar-4')){ dynamic_sidebar('sidebar-4'); } ?>
 			</div>
 		</div>
