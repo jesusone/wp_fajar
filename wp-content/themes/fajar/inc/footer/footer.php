f0cfbb88a365943bbca1dcb419cc83b1647035c5
Hoang Vu <nguyenhoangvu1803@gmail.com>
2016-03-19 15:08:38 +0700

    up

--- wp-content/themes/fajar/inc/footer/footer.php
+++ wp-content/themes/fajar/inc/footer/footer.php
@@ -53,7 +53,7 @@
 			</div>
 		</div>
 	</div>
-	<div id="yeah-footer-bottom">
+	<div class="yeah-footer-bottom">
 		<div class="container">
 			<div class="row">				
 				<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 logo">
