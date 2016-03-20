87fd71d13e1215c6fe41114295304dfe53efbe02
Hoang Vu <nguyenhoangvu1803@gmail.com>
2016-03-20 11:14:04 +0700

    up

--- wp-content/themes/fajar/single-templates/content/content.php
+++ wp-content/themes/fajar/single-templates/content/content.php
@@ -17,30 +17,38 @@ if( basename($template) === 'blog-classic.php') {
 }
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class('post-item'); ?>>
-    <?php if(has_post_thumbnail()) : ?>
-    <div class="yeah-blog-image">
-        <a title="<?php the_title(); ?>" href="<?php the_permalink() ?>" rel=""><?php the_post_thumbnail( $zo_image_size ); ?></a>
-    </div>
+    
+	<?php if(has_post_thumbnail()) : ?>
+		<!-- Thumb -->
+		<div class="yeah-blog-image">
+			<a title="<?php the_title(); ?>" href="<?php the_permalink() ?>" rel=""><?php the_post_thumbnail( $zo_image_size ); ?></a>
+		</div>
     <?php endif ?>
-
+	
+	<!-- Title -->
+	<h2 class="yeah-blog-title"><a title="<?php the_title(); ?>" href="<?php the_permalink() ?>" rel=""><?php the_title(); ?></a></h2>
+	
+	<!-- Content -->
+	<div class="yeah-blog-content">
+		<?php
+		if(get_post_type( get_the_ID() ) != 'page'):
+			the_excerpt();
+		endif;
+		wp_link_pages( array(
+			'before'      => '<p class="page-links"><span class="page-links-title">' . __( 'Pages:', 'fajar' ) . '</span>',
+			'after'       => '</p>',
+			'link_before' => '<span>',
+			'link_after'  => '</span>',
+			'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'fajar' ) . ' </span>%',
+			'separator'   => '<span class="screen-reader-text">, </span>',
+		) );
+		?>
+	</div>
+	
     <div class="yeah-blog-detail">
-        <h2 class="yeah-blog-title"><a title="<?php the_title(); ?>" href="<?php the_permalink() ?>" rel=""><?php the_title(); ?></a></h2>
+        
         <div class="yeah-blog-meta"><?php zo_archive_detail(); ?></div>
-        <div class="yeah-blog-content">
-            <?php
-            if(get_post_type( get_the_ID() ) != 'page'):
-                the_excerpt();
-            endif;
-			wp_link_pages( array(
-				'before'      => '<p class="page-links"><span class="page-links-title">' . __( 'Pages:', 'fajar' ) . '</span>',
-				'after'       => '</p>',
-				'link_before' => '<span>',
-				'link_after'  => '</span>',
-				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'fajar' ) . ' </span>%',
-				'separator'   => '<span class="screen-reader-text">, </span>',
-			) );
-			?>
-        </div>
+        
         <a class="btn-readmore" title="<?php the_title(); ?>" href="<?php the_permalink() ?>" rel=""><?php _e('Read More ', 'fajar') ?></a>
     </div>
 </article>
