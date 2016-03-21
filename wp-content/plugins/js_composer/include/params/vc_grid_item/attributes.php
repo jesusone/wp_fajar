276e40d31898e3260bb66c0aecccd96fa6b9d6dd
Hoang Vu <nguyenhoangvu1803@gmail.com>
2016-03-21 19:46:00 +0700

    up

--- wp-content/plugins/js_composer/include/params/vc_grid_item/attributes.php
+++ wp-content/plugins/js_composer/include/params/vc_grid_item/attributes.php
@@ -161,7 +161,7 @@ function vc_gitem_template_attribute_post_image_url_attr_prettyphoto( $value, $d
 		'data' => '',
 	), $data ) );
 	$href = vc_gitem_template_attribute_post_image_url_href( $value, array( 'post' => $post, 'data' => '' ) );
-	$rel = ' rel="' . esc_attr( 'prettyPhoto[rel-' . md5( vc_request_param( 'shortcode_id' ) ) . ']' ) . '"';
+	$rel = ' data-rel="' . esc_attr( 'prettyPhoto[rel-' . md5( vc_request_param( 'shortcode_id' ) ) . ']' ) . '"';
 	return $href . $rel . ' class="' . esc_attr( $data . ( strlen( $href ) ? ' prettyphoto' : '' ) )
 	       . '" title="' . esc_attr(
 		   apply_filters( 'vc_gitem_template_attribute_post_title', $post->post_title, $data_default ) ) . '"';
