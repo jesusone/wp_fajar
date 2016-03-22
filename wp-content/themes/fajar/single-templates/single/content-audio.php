320134ee7e579933eb74f3046efdb55f5544dd39
Hoang Vu <nguyenhoangvu1803@gmail.com>
2016-03-22 20:05:59 +0700

    up

--- wp-content/themes/fajar/single-templates/single/content-audio.php
+++ wp-content/themes/fajar/single-templates/single/content-audio.php
@@ -9,29 +9,30 @@
  */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class('post-item'); ?>>
-    <div class="yeah-blog-image yeah-blog-audio">
+	<!-- Audio -->
+    <div class="yeah-blog-audio">
         <?php echo zo_archive_audio(); ?>
     </div>
-
+	
+	<!-- Meta -->
+	<div class="yeah-blog-meta"><?php zo_archive_detail(); ?></div>
+	
+	<!-- Title -->
+	<h2 class="yeah-blog-title"><?php the_title(); ?></h2>
+	
+	<!-- Detail -->
     <div class="yeah-blog-detail">
-        <h2 class="yeah-blog-title"><?php the_title(); ?></h2>
-        <div class="yeah-blog-meta"><?php zo_archive_detail(); ?></div>
         <div class="yeah-blog-content">
-            <?php
-            if(zo_archive_audio()) {
-                echo apply_filters('the_content', preg_replace(array('/\[embed(.*)](.*)\[\/embed\]/', '/\[audio(.*)](.*)\[\/audio\]/'), '', get_the_content(), 1));
-            } else {
-                the_excerpt();
-            }
-            wp_link_pages( array(
-                'before'      => '<p class="page-links"><span class="page-links-title">' . __( 'Pages:', 'fajar' ) . '</span>',
-                'after'       => '</p>',
-                'link_before' => '<span>',
-                'link_after'  => '</span>',
-                'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'fajar' ) . ' </span>%',
-                'separator'   => '<span class="screen-reader-text">, </span>',
-            ) );
-            ?>
+            <?php the_content();
+			wp_link_pages( array(
+				'before'      => '<p class="page-links"><span class="page-links-title">' . __( 'Pages:', 'fajar' ) . '</span>',
+				'after'       => '</p>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'fajar' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+			?>
         </div>
     </div>
 </article>
--- wp-content/themes/fajar/single-templates/single/content-gallery.php
+++ wp-content/themes/fajar/single-templates/single/content-gallery.php
@@ -9,29 +9,30 @@
  */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class('post-item'); ?>>
-    <div class="yeah-blog-image yeah-blog-gallery">
+	<!-- Gallery -->
+    <div class="yeah-blog-gallery">
         <?php echo zo_archive_gallery( 'full' ); ?>
     </div>
-
+	
+	<!-- Meta -->
+	<div class="yeah-blog-meta"><?php zo_archive_detail(); ?></div>
+	
+	<!-- Title -->
+	<h2 class="yeah-blog-title"><?php the_title(); ?></h2>
+	
+	<!-- Detail -->
     <div class="yeah-blog-detail">
-        <h2 class="yeah-blog-title"><?php the_title(); ?></h2>
-        <div class="yeah-blog-meta"><?php zo_archive_detail(); ?></div>
         <div class="yeah-blog-content">
-            <?php
-            if(zo_archive_gallery()){
-                echo apply_filters('the_content', preg_replace('/\[gallery.*ids=.(.*).\]/', '', get_the_content()));
-            } else {
-                the_content();
-            }
-            wp_link_pages( array(
-                'before'      => '<p class="page-links"><span class="page-links-title">' . __( 'Pages:', 'fajar' ) . '</span>',
-                'after'       => '</p>',
-                'link_before' => '<span>',
-                'link_after'  => '</span>',
-                'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'fajar' ) . ' </span>%',
-                'separator'   => '<span class="screen-reader-text">, </span>',
-            ) );
-            ?>
+            <?php the_content();
+			wp_link_pages( array(
+				'before'      => '<p class="page-links"><span class="page-links-title">' . __( 'Pages:', 'fajar' ) . '</span>',
+				'after'       => '</p>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'fajar' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+			?>
         </div>
     </div>
 </article>
