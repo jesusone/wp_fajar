b3610361c15c28cb3628387a05084846a731768c
jesusone <ngocchaucit@gmail.com>
2016-04-02 23:23:51 +0700

    update

--- wp-content/plugins/yeah-woo-deals/admin/admin.module.class.php
+++ wp-content/plugins/yeah-woo-deals/admin/admin.module.class.php
@@ -298,7 +298,7 @@ IN ( ".esc_sql($data['category'])." )) ";
                 array(
                     'key' => '_yeah_price_sale',
                 ),
-                $arrays,
+              //  $arrays,
 
 
             ),
@@ -315,7 +315,7 @@ IN ( ".esc_sql($data['category'])." )) ";
             $sale_off['min'] = ($price_sale * 100) / $price_regular;
         }
         if($posts_max){
-            var_dump($posts_max->posts[0]->ID);
+
             $meta_fields = get_post_custom($posts_max->posts[0]->ID);
             $price_sale = get_post_meta($posts_max->posts[0]->ID,'_yeah_price_sale',true);
             $price_regular = get_post_meta($posts_max->posts[0]->ID,'_regular_price',true);
