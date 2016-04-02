4a4a553b936d9077730b165d823d43c6a1d088e6
jesusone <ngocchaucit@gmail.com>
2016-04-02 14:11:56 +0700

    update

--- wp-content/plugins/yeah-woo-deals/admin/admin.class.php
+++ wp-content/plugins/yeah-woo-deals/admin/admin.class.php
@@ -15,11 +15,11 @@ class YeahWooDealsAdmin {
         add_action('wp_enqueue_scripts', array($this, 'add_admin_script'));
 
         // add admin page.
-        add_action ( 'admin_menu', array ($this, 'add_admin_page'));
+        /*add_action ( 'admin_menu', array ($this, 'add_admin_page'));
 
         add_filter( 'woocommerce_product_data_tabs', array( &$this, 'yeah_product_tab' ) );
         add_action( 'woocommerce_product_data_panels', array( &$this, 'yeah_product_tab_content' ) );
-        add_action( 'save_post', array( &$this,'yeah_update_custom_meta_fields' ) );
+        add_action( 'save_post', array( &$this,'yeah_update_custom_meta_fields' ) );*/
         $this->yeah_get_group_deals();
     }
     function yeah_product_tab_content(){
