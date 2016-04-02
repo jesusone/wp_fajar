54c561e00225d1edc0a9721be6402e0a50303eee
jesusone <ngocchaucit@gmail.com>
2016-04-02 14:20:54 +0700

    update

--- wp-content/plugins/yeah-woo-deals/yeah-woo-deals.php
+++ wp-content/plugins/yeah-woo-deals/yeah-woo-deals.php
@@ -78,7 +78,7 @@ if (! class_exists('YeahWooDeals')) {
             /*Include class admin*/
             require_once $this->plugin_dir . 'includes/core-functions.php';
             require_once $this->plugin_dir . 'includes/helper.class.php';
-            require_once $this->plugin_dir . 'admin/admin.class.php';
+           // require_once $this->plugin_dir . 'admin/admin.class.php';
             require_once $this->plugin_dir . 'admin/admin.module.class.php';
             require_once $this->plugin_dir . 'includes/widgets/class.widgets.php';
         }
