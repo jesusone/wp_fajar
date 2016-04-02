b4397d62c55410ede53a60302460a1fad0dfc177
jesusone <ngocchaucit@gmail.com>
2016-04-02 20:07:07 +0700

    update

--- wp-content/plugins/yeah-woo-deals/admin/templates/yeah-woo-deals.php
+++ wp-content/plugins/yeah-woo-deals/admin/templates/yeah-woo-deals.php
@@ -2,7 +2,7 @@
 $tablename = "yeah_woo_deals_short_code";
 global $wpdb;
 $deals = new  YeahWooDealsAdminModule();
-$result =$deals->yeah_get_data_widget();
+$results =$deals->yeah_get_data_widget();
 ?>
 <div class="yeah-message">
     <div class="yeah-message-content"></div>
