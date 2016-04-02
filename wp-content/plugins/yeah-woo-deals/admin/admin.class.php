51098507ebcf53a48b665c772decdab8b1e57975
jesusone <ngocchaucit@gmail.com>
2016-04-02 14:12:25 +0700

    update

--- wp-content/plugins/yeah-woo-deals/admin/admin.class.php
+++ wp-content/plugins/yeah-woo-deals/admin/admin.class.php
@@ -12,7 +12,7 @@ class YeahWooDealsAdmin {
         // wp head.
         add_action('admin_head', array($this, 'add_ajax_url'));
         // admin scripts.
-        add_action('wp_enqueue_scripts', array($this, 'add_admin_script'));
+      //  add_action('wp_enqueue_scripts', array($this, 'add_admin_script'));
 
         // add admin page.
         /*add_action ( 'admin_menu', array ($this, 'add_admin_page'));
