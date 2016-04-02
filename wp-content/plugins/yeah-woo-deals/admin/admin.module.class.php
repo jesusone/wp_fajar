6b93082841e7d2afa853dc24bf23b7ca55cdae29
jesusone <ngocchaucit@gmail.com>
2016-04-02 22:52:04 +0700

    update

--- wp-content/plugins/yeah-woo-deals/admin/admin.module.class.php
+++ wp-content/plugins/yeah-woo-deals/admin/admin.module.class.php
@@ -287,6 +287,8 @@ IN ( ".esc_sql($data['category'])." )) ";
         if($posts_min){
            $price_sale = get_post_meta($posts_min->posts[0]->ID,'_yeah_price_sale',true);
            $price_regular = get_post_meta($posts_min->posts[0]->ID,'_regular_price',true);
+            var_dump($price_sale);
+            var_dump($price_regular);
             $sale_off['min'] = ($price_sale * 0.01) / $price_regular;
         }
         var_dump($sale_off);
