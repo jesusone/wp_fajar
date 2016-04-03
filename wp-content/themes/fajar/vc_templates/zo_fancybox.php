05e070565993877db6119ff6b7574eecac1272fb
jesusone <ngocchaucit@gmail.com>
2016-04-03 15:30:32 +0700

    up

--- wp-content/themes/fajar/vc_templates/zo_fancybox.php
+++ wp-content/themes/fajar/vc_templates/zo_fancybox.php
@@ -31,7 +31,7 @@ if($yeah_carousel == 'yes'){
 ?>
 <div class="<?php echo esc_attr($class_link); ?> zo-fancyboxes-wraper zo-fancybox-default <?php echo esc_attr($atts['template']); ?>" id="<?php echo esc_attr($atts['html_id']); ?>">
     <?php if ($atts['title'] != ''): ?>
-        <div class="zo-fancyboxes-head">
+        <div class="yeah-fancyboxes-head">
             <div class="zo-fancyboxes-title">
                 <?php echo apply_filters('the_title', $atts['title']); ?>
             </div>
@@ -61,7 +61,7 @@ if($yeah_carousel == 'yes'){
                     $image_url = $attachment_image[0];
                 }
                 ?>
-                <div class="zo-fancybox-item <?php echo esc_attr($atts['item_class']); ?>">
+                <div class="yeah-fancybox-item <?php echo esc_attr($atts['item_class']); ?>">
                     <div class="zo-fancybox-inner">
                         <?php if ($image_url): ?>
                             <div class="zo-fancybox-image">
@@ -72,25 +72,27 @@ if($yeah_carousel == 'yes'){
                                 <i class="<?php echo esc_attr($icon); ?>" style="color: <?php echo esc_attr($zo_fancybox_icon_color); ?>;"></i>
                             </div>
                         <?php endif; ?>
-                        <?php if ($title): ?>
-                            <div class="zo-fancybox-content-title">
-                                <<?php echo esc_attr($zo_title_size); ?> class="zo-fancybox-title" style="color: <?php echo esc_attr($zo_fancybox_title_color); ?>;"> <?php echo apply_filters('the_title', $title); ?></<?php echo esc_attr($zo_title_size); ?>>
-                            </div>
-                        <?php endif; ?>
-                        <?php if( $content) : ?>
-                            <div class="zo-fancybox-content" style="color: <?php echo esc_attr($zo_fancybox_content_color); ?>;">
-                                <?php echo apply_filters('the_content', $content); ?>
-                            </div>
-                        <?php endif ;?>
+                        <div class="yeah-fancybox-details">
+                            <?php if ($title): ?>
+                                <div class="yeah-fancybox-content-title">
+                                    <<?php echo esc_attr($zo_title_size); ?> class="zo-fancybox-title" style="color: <?php echo esc_attr($zo_fancybox_title_color); ?>;"> <?php echo apply_filters('the_title', $title); ?></<?php echo esc_attr($zo_title_size); ?>>
+                                </div>
+                            <?php endif; ?>
+                            <?php if( $content) : ?>
+                                <div class="yeah-fancybox-content" style="color: <?php echo esc_attr($zo_fancybox_content_color); ?>;">
+                                    <?php echo apply_filters('the_content', $content); ?>
+                                </div>
+                            <?php endif ;?>
 
-                        <?php if($atts['button_text']!=''):?>
-                            <div class="zo-fancyboxes-readmore">
-                                <?php
-                                $class_btn = ($atts['button_type']=='button')?'btn btn-large':'';
-                                ?>
-                                <a href="<?php echo esc_url($button_link);?>" class="<?php echo esc_attr($class_btn);?>"><?php echo esc_attr($atts['button_text']);?></a>
-                            </div>
-                        <?php endif;?>
+                            <?php if($atts['button_text']!=''):?>
+                                <div class="yeah-fancyboxes-readmore">
+                                    <?php
+                                    $class_btn = ($atts['button_type']=='button')?'btn btn-large':'';
+                                    ?>
+                                    <a href="<?php echo esc_url($button_link);?>" class="<?php echo esc_attr($class_btn);?>"><?php echo esc_attr($atts['button_text']);?></a>
+                                </div>
+                            <?php endif;?>
+                        </div>
                     </div>
                 </div>
 
