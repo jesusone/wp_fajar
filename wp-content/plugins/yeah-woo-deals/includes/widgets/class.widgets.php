55312b9877c3cfccb4bf67a1f4414f02628ca667
jesusone <ngocchaucit@gmail.com>
2016-04-03 16:01:41 +0700

    up

--- wp-content/plugins/yeah-woo-deals/includes/widgets/class.widgets.php
+++ wp-content/plugins/yeah-woo-deals/includes/widgets/class.widgets.php
@@ -27,56 +27,7 @@ if (! class_exists('Yeah_Woo_Deal_widget')) {
         {
             echo $args['before_widget'];
 
-            if (! empty($instance['title'])) {
-                echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
-            }
-            $yeah_group = !empty($instance['group_deals']) ? $instance['group_deals'] : '';
-            $image_title = !empty($instance['image_title']) ? $instance['image_title'] : '';
-            $module = new YeahWooDealsAdminModule();
-          //  $posts = $module->yeah_get_data_widget($yeah_group);
-            $sale_off = $module->yeah_get_sale_off($yeah_group);
-            $current_datetimes = date('Y/m/d H:i:s');
-            while ( $posts->have_posts() ) {
-                $posts->the_post();
-                $meta_fields = get_post_custom($posts->ID);
-                $dates = ( isset( $meta_fields['_yeah_woo_dates'] ) )? unserialize( $meta_fields['_yeah_woo_dates'][0] ) : "";
-                $date_now = date( 'Y/m/d H:i:s' );
-                $date_start = date( $dates['start'] );
-                $date_end = date( $dates['end'] );
-
-                $date_countdown = $dates['end'];
-                $_extra_class = array();
-                $_extra_data = array('date_start' => $dates['start'],
-                    'date_end' => $dates['end'],
-                    'view' => 'list');
-
-                if( $date_start > $current_datetimes ) {
-                    array_push( $_extra_class, 'wa-product-not-start' );
-                    $date_countdown = $dates['start'];
-                    $_extra_data['status'] = 'close';
-                }else {
-                    $_extra_data['status'] = 'open';
-                }
-                ?>
-                <div class="yeah-weekend-deals-widget-top">
-                    <img src="<?php echo esc_url($image_title);?>"/>
-                    <h3 class="museo_slab_500"><?php echo esc_attr($sale_off['min'])?><?php echo esc_html__('% -','yeah-woo-deals'); ?> <?php echo esc_attr($sale_off['max'])?><?php echo esc_html__('% off','yeah-woo-deals') ?></h3>
-                </div>
-                <!--Start CountDown-->
-                <!---->
-                <div class="yeah-countdown-content">
-                    <h2 class="yeah-title-countdown"><?php echo esc_html__('Time Left','yeah-woo-deals') ?></h2>
-                    <div class="countdown yeah-countdown-inner" data-extradata="<?php echo esc_attr( json_encode( $_extra_data ) ); ?>" data-datatime="<?php echo "{$date_countdown}"; ?>"  data-format='<div>%-d<span>day%!d</span></div><div>%H<span>hrs</span></div><div>%M<span>min</span></div><div>%S<span>sec</span></div>'>
-                        <div class="clock"></div>
-                    </div>
-                </div>
-                <!--End CountDown-->
-                <?php
-            }
-
-            ?>
-
-            <?php
+         
             echo $args['after_widget'];
         }
 
