7b383eb98de6cc0c4f764bc64f897d71e7b937be
jesusone <ngocchaucit@gmail.com>
2016-04-06 23:49:58 +0700

    up

--- wp-content/themes/fajar/woocommerce/loop/add-to-cart.php
+++ wp-content/themes/fajar/woocommerce/loop/add-to-cart.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $product;
 
 echo apply_filters( 'woocommerce_loop_add_to_cart_link',
-	sprintf( '<div class="yeah-add-to-cart"><a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" data-quantity="%s" class="%s product_type_%s product-cart-btn pull-left " title="%s"><i class="icon-icons240"></i><span>%s</span></a><a href="shop-detail.html" title="'.esc_html__('Details','woocommerce').'" class="product-detail-btn pull-right"><i class="icon-list3"></i> '.esc_html__('Details','woocommerce').'</a></div>',
+	sprintf( '<div class="yeah-add-to-cart"><a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" data-quantity="%s" class="%s product_type_%s product-cart-btn pull-left " title="%s"><i class="icon-icons240"></i><span>%s</span></a><a href="'.the_permalink().'" title="'.esc_html__('Details','woocommerce').'" class="product-detail-btn pull-right"><i class="icon-list3"></i> '.esc_html__('Details','woocommerce').'</a></div>',
 		esc_url( $product->add_to_cart_url() ),
 		esc_attr( $product->id ),
 		esc_attr( $product->get_sku() ),
--- wp-content/themes/fajar/woocommerce/wc-template-functions.php
+++ wp-content/themes/fajar/woocommerce/wc-template-functions.php
@@ -48,10 +48,4 @@ function zo_custom_shipping_free_label( $label ) {
 }
 add_filter( 'woocommerce_cart_shipping_method_full_label' , 'zo_custom_shipping_free_label' );
 
-//Add Ajax sort
-add_action( 'woocommerce_before_main_content', 'yeah_feauter_products', 20 );
-function yeah_feauter_products(){
-    ?>
-    <h1>Add Feauter</h1>
-<?php
-}
+
