f0e6b26a29f305a4e8897ac6f4ac1ee53e53dcbd
jesusone <ngocchaucit@gmail.com>
2016-04-12 23:48:31 +0700

    update

--- wp-content/themes/fajar/page-templates/no-sidebar.php
+++ wp-content/themes/fajar/page-templates/no-sidebar.php
@@ -18,68 +18,7 @@ get_header( 'shop' ); ?>
         <main id="main" class="site-main" role="main">
             <div class="<?php echo get_post_meta(get_the_ID(), 'zo_layout', true) ? 'no-container' : 'container'; ?>">
                 <div class="row">
-                    <div class=" col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <?php
-                        /**
-                         * woocommerce_before_main_content hook
-                         *
-                         * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-                         * @hooked woocommerce_breadcrumb - 20
-                         */
-                        remove_action('woocommerce_before_main_content', 'woocommerce_breadcrumb', 20);
-                        do_action( 'woocommerce_before_main_content' );
-                        ?>
 
-                        <?php do_action( 'woocommerce_archive_description' ); ?>
-
-                        <?php if ( have_posts() ) : ?>
-
-                            <?php
-                            /**
-                             * woocommerce_before_shop_loop hook
-                             *
-                             * @hooked woocommerce_result_count - 20
-                             * @hooked woocommerce_catalog_ordering - 30
-                             */
-                            do_action( 'woocommerce_before_shop_loop' );
-                            ?>
-
-                            <?php woocommerce_product_loop_start(); ?>
-
-                            <?php woocommerce_product_subcategories(); ?>
-
-                            <?php while ( have_posts() ) : the_post(); ?>
-
-                                <?php wc_get_template_part( 'content', 'product' ); ?>
-
-                            <?php endwhile; // end of the loop. ?>
-
-                            <?php woocommerce_product_loop_end(); ?>
-
-                            <?php
-                            /**
-                             * woocommerce_after_shop_loop hook
-                             *
-                             * @hooked woocommerce_pagination - 10
-                             */
-                            do_action( 'woocommerce_after_shop_loop' );
-                            ?>
-
-                        <?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
-
-                            <?php wc_get_template( 'loop/no-products-found.php' ); ?>
-
-                        <?php endif; ?>
-
-                        <?php
-                        /**
-                         * woocommerce_after_main_content hook
-                         *
-                         * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-                         */
-                        do_action( 'woocommerce_after_main_content' );
-                        ?>
-                    </div>
                 </div>
             </div>
         </main><!-- #main -->
