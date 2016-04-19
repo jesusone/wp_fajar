26f4e0dfecadc4a39ac4dd81f6deb4647cc66f6b
Hoang Vu <nguyenhoangvu1803@gmail.com>
2016-04-19 23:22:59 +0700

    up

---
+++ wp-content/themes/fajar/vc_templates/zo_grid--lastest-blog.php
@@ -0,0 +1,95 @@
+<?php 
+    /* get categories */
+        $taxo = 'category';
+        $_category = array();
+        if(!isset($atts['cat']) || $atts['cat']==''){
+            $terms = get_terms($taxo);
+            foreach ($terms as $cat){
+                $_category[] = $cat->term_id;
+            }
+        } else {
+            $_category  = explode(',', $atts['cat']);
+        }
+        $atts['categories'] = $_category;
+?>
+<div class="yeah-grid-wrapper <?php echo esc_attr($atts['template']);?>" id="<?php echo esc_attr($atts['html_id']);?>">
+
+	<!-- Get Filter Query -->
+	<?php if (isset($atts['filter']) && $atts['filter'] == 1 && $atts['layout'] == 'masonry'): ?>
+        <div class="yeah-grid-filter">
+            <ul class="yeah-filter-category list-unstyled list-inline">
+                <li><a class="active" href="#" data-group="all"><?php esc_html_e("All", 'fajar');?></a></li>
+				<?php
+					$posts = $atts['posts'];
+					$query = $posts->query;
+					$taxs  = array();
+					if(isset($query['tax_query'])){
+						$tax_query=$query['tax_query'];
+						foreach($tax_query as $tax){
+							if(is_array($tax)){
+								$taxs[] = $tax['terms'];
+							}
+						}
+					}
+					foreach ($atts['categories'] as $category):
+						if(!empty($taxs)){
+							if(in_array($category,$taxs)) {
+								$term = get_term($category, $taxonomy); 
+					?>
+								<li><a href="#" data-group="<?php echo esc_attr('category-' . $term->slug); ?>"><?php echo esc_attr($term->name); ?></a></li>
+				<?php 		}
+						}else{
+							$term = get_term($category, $taxonomy); 
+				?>
+							<li><a href="#" data-group="<?php echo esc_attr('category-' . $term->slug); ?>"><?php echo esc_attr($term->name); ?></a></li>
+				<?php
+						} 
+					endforeach; 
+				?>
+            </ul>
+        </div>
+    <?php endif; ?>
+	
+    <div class="row zo-grid <?php echo esc_attr($atts['grid_class']);?>">
+        <?php
+        $posts = $atts['posts'];
+        while($posts->have_posts()){
+            $posts->the_post();
+            $groups = array();
+            $groups[] = '"all"';
+            foreach(zoGetCategoriesByPostID(get_the_ID(),$taxo) as $category){
+                $groups[] = '"category-'.$category->slug.'"';
+            }
+            ?>
+            <div class="zo-grid-item <?php echo esc_attr($atts['item_class']);?>" data-groups='[<?php echo implode(',', $groups);?>]'>
+                <?php 
+                    if(has_post_thumbnail() && !post_password_required() && !is_attachment() &&  wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID()), 'full', false)):
+                        $class = ' has-thumbnail';
+                        $thumbnail = zo_post_thumbnail(get_the_ID(),626,448,true,true,true);
+                    else:
+                        $class = ' no-image';
+                        $thumbnail = '<img src="'.ZO_IMAGES.'no-image.jpg" alt="'.get_the_title().'" />';
+                    endif;
+                ?>
+				<div class="zo-grid-media <?php echo esc_attr($class);?>">
+					<?php print_r($thumbnail);?>
+					<div class="zo-grid-detail">
+						<div class="zo-grid-time">
+							<?php the_time('d, M Y');?>
+						</div>
+						<div class="zo-grid-title">
+							<?php the_title();?>
+						</div>
+						<div class="author"><?php esc_html_e('By', 'fajar');?><?php the_author_posts_link(); ?></div>
+						<a href="<?php the_permalink(); ?>"><?php echo comments_number('0','1','%'); ?> <?php esc_html_e('Comments','fajar');?></a>
+					</div>
+				</div>
+            </div>
+            <?php
+        }
+        ?>
+    </div>
+    <?php
+    wp_reset_postdata();
+    ?>
+</div>
