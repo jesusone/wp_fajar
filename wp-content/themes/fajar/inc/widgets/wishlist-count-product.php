956fb6dafbe726d72bfaa063f95c37bf6ce31e7b
Hoang Vu <nguyenhoangvu1803@gmail.com>
2016-04-22 22:49:53 +0700

    up

---
+++ wp-content/themes/fajar/inc/widgets/wishlist-count-product.php
@@ -0,0 +1,63 @@
+<?php
+class WC_Widget_Wishlist_Count_Product extends WP_Widget {
+	
+    public function __construct() {
+        parent::__construct(
+            'widget_wishlist_count_product', // Base ID
+            __('Wishlist Count Product', 'fajar'), // Name
+            array('description' => __("Display link wishlist & count product in your wishlist.", 'fajar'),) // Args
+        );
+    }
+	
+	/* FRONT END */
+    function widget($args, $instance) {
+        extract(shortcode_atts($instance,$args));
+		$title = apply_filters('widget_title', empty( $instance['title'] ) ?'' : $instance['title'], $instance, $this->id_base );
+        $icon = isset($instance['icon']) ? $instance['icon'] : '';
+        ?>
+			<div class="widget_wishlist_count_product">
+				<?php 
+					if(!empty($icon)){
+						echo "<i class='" . esc_attr($icon) . "'></i>";
+					} 
+					if(!empty($title)){
+						echo "<div class='wishlist-page'>" . do_shortcode('[yith_wcwl_wishlist]') . "</div>";
+					}
+				?>
+			</div>
+		<?php
+        echo ob_get_clean();
+    }
+	
+	/* BACKEND - UPDATE */
+    function update( $new_instance, $old_instance ) {
+        $instance = $old_instance;
+        $instance['title'] = $new_instance['title'];
+        $instance['icon'] = $new_instance['icon'];
+		return $instance;
+    }
+	
+	/* BACKEND - FORM */
+    function form( $instance ) {
+        $title = isset($instance['title']) ? esc_attr($instance['title']) : '';
+        $icon = isset($instance['icon']) ? $instance['icon'] : 0;
+        ?>
+        <p>
+            <label for="<?php echo esc_attr($this->get_field_id('title')); ?>"><?php _e( 'Title:', 'fajar' ); ?></label>
+            <input id="<?php echo esc_attr( $this->get_field_id('title') ); ?>" name="<?php echo esc_attr( $this->get_field_name('title') ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+        </p>
+		<p>
+            <label for="<?php echo esc_url($this->get_field_id('icon')); ?>"><?php _e( 'Enter class icon:', 'fajar' ); ?></label>
+			<input id="<?php echo esc_attr( $this->get_field_id('icon') ); ?>" name="<?php echo esc_attr( $this->get_field_name('icon') ); ?>" type="text" value="<?php echo esc_attr( $icon ); ?>" />
+        </p>
+    <?php
+
+    }
+}
+
+function register_wishlist_count_product_widget() {
+    register_widget('WC_Widget_Wishlist_Count_Product');
+}
+add_action('widgets_init', 'register_wishlist_count_product_widget');
+?>
+?>
